10d5823a55db54a59c28dcc1df06aafbe713c4b0
fernal73 <linus.fernandes@gmail.com>
2019-03-16 10:49:57 +0530

    Updated reverse array

--- ReverseArrayFixed.c
+++ ReverseArrayFixed.c
@@ -23,13 +23,9 @@ int main() { // int array with 6 elements
   for (int i = 0; i < count / 2; i++) {
 
     arr1[i] = arr1[i]^arr1[count - i - 1];
-    printf("%d",arr1[i]);
         arr1[count -i -1]=arr1[i]^arr1[count -i -1];
-        printf("%d",arr1[count-i-1]);
     arr1[i]=arr1[i]^arr1[count -i -1];
-    printf("%d",arr1[i]);
-
-  }
+      }
   printf("Original after reversing again: ");
   for (int    i = 0; i < count; i++) {
     printf("%d ", arr1[i]);
