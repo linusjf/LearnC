6f696fcacd7721d9c2e93d75dca54dea63fde0ad
fernal73 <linus.fernandes@gmail.com>
2019-03-16 07:05:59 +0530

    Added four pointer examples.

---
+++ PointersAndFunctions1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void test(int *num) {
+  *num += 6;
+  printf("num inside test: %d\n", *num);
+}
+int main() {
+  int num = 7;
+  printf("num before test: %d\n", num);
+  test(&num);
+  printf("num aftertest: %d\n", num);
+  return 0;
+}
