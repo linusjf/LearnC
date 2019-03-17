2e99eac85e484b305a11873fe2fe4198846924ac
fernal73 <linus.fernandes@gmail.com>
2019-03-17 15:05:11 +0530

    Added two structs examples.

---
+++ EmployeeStructs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct  {
+  char fname[50];
+  char lname[50];
+  char title[100];
+  int emp_id;
+} Employee;
+int main() {
+  Employee emp1;
+  strcpy(emp1.fname, "John");
+  strcpy(emp1.lname, "Smith");
+  strcpy(emp1.title, "Developer");
+  emp1.emp_id = 2000;
+  printf("First name: %s\n", emp1.fname);
+  printf("Last name: %s\n", emp1.lname);
+  printf("Title: %s\n", emp1.title);
+  printf("Employee id: %d\n", emp1.emp_id);
+  Employee *empptr = &emp1;
+  printf("\nPointer of Employee:\n");
+  printf("First name: %s\n", empptr->fname);
+  printf("Last name: %s\n", empptr->lname);
+  printf("Title: %s\n", empptr->title);
+  printf("Employee id: %d\n", empptr->emp_id);
+}
---
+++ NestedStructs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct {
+  char street[50];
+  char city[50];
+} Address;
+typedef struct {
+  int id;
+  char name[20];
+  char title[20];
+  Address address;
+} Employee;
+int main() {
+  Employee emp = {1000, "John Smith", "Developer", "123 Main Street", "Chicago"};
+  printf("Emp Id: %d \n", emp.id);
+  printf("Name: %s \n", emp.name);
+  printf("Title: %s \nAddress:\n", emp.title);
+  printf("Street: %s\n", emp.address.street);
+  printf("City: %s\n", emp.address.city);
+  return 0;
+}
