36311b57ea00325bb4f0099e3729f52e68a5260e
fernal73 <linus.fernandes@gmail.com>
2019-03-21 10:35:38 +0530

    Updated file to add two numbers using bit operations.

--- BitOperations.c
+++ BitOperations.c
@@ -4,12 +4,13 @@ int main() {
   // binary literals are a non-standard extension
   int x1 = 0b11001001;
   int x2 = 0b10111000;
-  int x3, x4, x5, x6,x7;
+  int x3, x4, x5, x6,x7,x8;
   x3 = x1 & x2;
   x4 = x1 | x2;
   x5 = x1 ^ x2;
   x6 = (x1 << 2);
   x7 = (x1 >> 1);
+  x8 = (x3 << 1);
   printf("x1 (0b11001000) = %x %d %o\n", x1, x1, x1);
   printf("x2 (0b10111000) = %x %d %o\n", x2, x2, x2);
   printf("x3 (x1 & x2) = %x %d %o\n", x3, x3, x3);
@@ -17,5 +18,8 @@ int main() {
   printf("x5 (x1 ^ x2) = %x %d %o\n", x5, x5, x5);
   printf("x6 (x1 << 2) = %x %d %o\n", x6, x6, x6);
   printf("x7 (x1 >> 1) = %x %d %o\n", x7, x7, x7);
+  printf("x8 (x3 << 1) = %x %d %o\n", x8, x8, x8);
+  printf("((x1 & x2) << 1) + (x1 ^ x2) = %x %d %o\n", x8+x5, x8+x5, x8+x5);
+
     return 0;
 }
