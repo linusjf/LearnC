19819278e9f7f3c9988f51800d3d5797a5e3e722
fernal73 <linus.fernandes@gmail.com>
2021-03-13 17:40:49 +0530

    Remove deadlock in example

--- PThreads/freelock.c
+++ PThreads/freelock.c
@@ -19,13 +19,17 @@ void foo_bar_inc() {
   P_M_U(&foo_mutex);
 }
 
-void bar_foo_dec() {
-  P_M_L(&bar_mutex);
-  sleep(1);
-  P_M_L(&foo_mutex);
-  foo = bar--;
-  P_M_U(&foo_mutex);
-  P_M_U(&bar_mutex);
+void bar_foo_dec()
+{
+    P_M_L(&bar_mutex);
+    while (pthread_mutex_trylock(&foo_mutex)) {
+	P_M_U(&bar_mutex);
+	sleep(1);
+        P_M_L(&bar_mutex);
+    }
+    foo = bar--;
+    P_M_U(&bar_mutex);
+    P_M_U(&foo_mutex);
 }
 
 void *threadone() {
@@ -42,11 +46,11 @@ int main(int argc, char *argv[]) {
   pthread_t thread1;
   pthread_t thread2;
 
-  printf("Deadlock example\n");
+  printf("Freelock example\n");
   pthread_create(&thread1, NULL, threadone, NULL);
   pthread_create(&thread2, NULL, threadtwo, NULL);
-  pthread_join(thread1,NULL);
+  pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
-  printf("Normal exit. Shouldn't reach here...\n");
+  printf("Normal exit.\n");
   pthread_exit(NULL);
 }
