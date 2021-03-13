94d57f035944c1b681f372df1183f9268430bc45
fernal73 <linus.fernandes@gmail.com>
2021-03-13 17:27:13 +0530

    Add deadlock example

---
+++ PThreads/deadlock.c
@@ -0,0 +1,52 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define P_M_L(x) pthread_mutex_lock(x)
+#define P_M_U(x) pthread_mutex_unlock(x)
+
+int num_pthreads = 2;
+pthread_mutex_t foo_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t bar_mutex = PTHREAD_MUTEX_INITIALIZER;
+int foo = 0, bar = 0;
+
+void foo_bar_inc() {
+  P_M_L(&foo_mutex);
+  sleep(1);
+  P_M_L(&bar_mutex);
+  bar = foo++;
+  P_M_U(&bar_mutex);
+  P_M_U(&foo_mutex);
+}
+
+void bar_foo_dec() {
+  P_M_L(&bar_mutex);
+  sleep(1);
+  P_M_L(&foo_mutex);
+  foo = bar--;
+  P_M_U(&foo_mutex);
+  P_M_U(&bar_mutex);
+}
+
+void *threadone() {
+  bar_foo_dec();
+  return NULL;
+}
+
+void *threadtwo() {
+  foo_bar_inc();
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t thread1;
+  pthread_t thread2;
+
+  printf("Deadlock example\n");
+  pthread_create(&thread1, NULL, threadone, NULL);
+  pthread_create(&thread2, NULL, threadtwo, NULL);
+  pthread_detach(thread1);
+  pthread_detach(thread2);
+  pthread_exit(NULL);
+  printf("Normal exit. Shouldn't reach here...\n");
+}
