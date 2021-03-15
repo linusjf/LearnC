ca208285ee6cf7c674d9e17e11e07e7ee29c8318
fernal73 <linus.fernandes@gmail.com>
2021-03-15 17:48:28 +0530

    Add openmp samples

---
+++ ForkJoin/andor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  fork();
+
+  (fork() && fork()) || fork();
+
+  fork();
+
+  printf("forked\n");
+
+  return 0;
+}
---
+++ OpenMP/critical.c
@@ -0,0 +1,17 @@
+ #include <omp.h>
+
+ int main(int argc, char *argv[]) {
+
+ int x;
+ x = 0;
+
+ #pragma omp parallel shared(x)
+   {
+
+   #pragma omp critical
+   x = x + 1;
+
+   }    /* end of parallel region */
+   printf("%d\n", x);
+ }
+
