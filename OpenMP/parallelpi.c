12a68e2f4ae4e72d642596f40f7f11cd280d8a23
fernal73 <linus.fernandes@gmail.com>
2021-03-16 06:09:10 +0530

    Add check to set thread count to number of logical cores

--- OpenMP/parallelpi.c
+++ OpenMP/parallelpi.c
@@ -1,27 +1,27 @@
 #include <omp.h>
-
-#define NUM_THREADS 6
+#include <unistd.h>
 
 static long num_steps = 100000;
 double step;
 
 int main(int argc, char *argv[]) {
   int i;
-  double pi = 0.0, sum[NUM_THREADS];
+  int nthrds = sysconf(_SC_NPROCESSORS_ONLN);
+  printf("Number of cores online: %d\n", nthrds);
+  double pi = 0.0, sum[nthrds];
   step = 1.0 / (double)num_steps;
-  omp_set_num_threads(NUM_THREADS);
+  omp_set_num_threads(nthrds);
 #pragma omp parallel
   {
-    int i, id, nthrds;
+    int i, id;
     double x;
     id = omp_get_thread_num();
-    nthrds = omp_get_num_threads();
-    for (i = id, sum[id] = 0.0; i < num_steps; i = i + nthrds) {
+    for (i = id, sum[id] = 0.0; i < num_steps; i += nthrds) {
       x = (i + 0.5) * step;
       sum[id] += 4.0 / (1.0 + x * x);
     }
   }
-  for (i = 0; i < NUM_THREADS; i++)
+  for (i = 0; i < nthrds; i++)
     pi += sum[i] * step;
   printf("PI = %f\n", pi);
 }
