997ae01f7e34117d8dbafca7adbd449c3ae0f79f
fernal73 <linus.fernandes@gmail.com>
2022-08-03 08:40:25 +0000

    feat: add endless loop program

---
+++ awe.c
@@ -0,0 +1,15 @@
+/*
+ * awe.c
+ * Copyright (C) 2022  <@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+void run_awesome_function() {
+  int x = 0;
+  while (1) {
+    x += 1;
+  }
+}
+
+int main() { run_awesome_function(); }
